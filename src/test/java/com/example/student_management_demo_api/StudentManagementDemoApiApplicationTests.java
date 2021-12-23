2bfea5baf296203c2b24a6bf0cb34188a7569324
Nguyen Khanh Nam <khanhnam3737@gmail.com>
2021-12-23 10:34:15 +0700

    git init

---
+++ src/test/java/com/example/student_management_demo_api/StudentManagementDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_management_demo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementDemoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
